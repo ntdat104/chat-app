b77112e5165b839b1943892a184fe911b7cf9620
Tiến Đạt <ntd.k108cva@gmail.com>
2020-10-03 17:20:10 +0700

    Updates

---
+++ precache-manifest.e4da75b237be3c252203beec6cb0bbeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20abd8d02ec7df353906443c71778714",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "6f7be3e25a37bf227d8f",
+    "url": "/chat-app/static/css/main.f85af530.chunk.css"
+  },
+  {
+    "revision": "581af08417e2ee8c9e7c",
+    "url": "/chat-app/static/js/2.ad125dcf.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chat-app/static/js/2.ad125dcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7be3e25a37bf227d8f",
+    "url": "/chat-app/static/js/main.05ecb591.chunk.js"
+  },
+  {
+    "revision": "a46df673de3fe3887809",
+    "url": "/chat-app/static/js/runtime-main.0c4eda3f.js"
+  }
+]);
